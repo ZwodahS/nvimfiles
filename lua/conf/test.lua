509cb3b8dce43fdf179e8485d19ef163795bc739
Eric Ng <ericnjf@gmail.com>
2023-04-07 10:21:51 +0800

    impl/autoclose: disable autoclose for text filetypes

--- lua/conf/test.lua
+++ lua/conf/test.lua
@@ -2,6 +2,8 @@ local m = require('conf.mappings')
 --[[
 For testing things unti putting them in proper file
 --]]
-require("autoclose").setup({})
+require("autoclose").setup({
+  disabled_filetypes = { "text", "markdown", "zdoc" },
+})
 
 m.nmap("<leader>a", ':lua require("autoclose").toggle()<CR>')
